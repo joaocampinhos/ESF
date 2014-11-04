9301bd47ff7d637d288caa71528660d5d751aebb
joaocampinhos <joao.campinhos@gmail.com>
2014-11-04 17:33:22 +0000

    Add handlebars and quick example

--- index.js
+++ index.js
@@ -1,15 +1,22 @@
+var Path = require('path');
 var Hapi = require('hapi');
 
 // Create a server with a host and port
 var server = new Hapi.Server('localhost', 8000);
+
+server.views({
+  engines: {
+    html: require('handlebars')
+  },
+  path: Path.join(__dirname, 'templates')
+});
 
 // Add the route
 server.route({
   method: 'GET',
   path: '/hello',
   handler: function (request, reply) {
-
-    reply('Empezamos!');
+    reply.view('index');
   }
 });
 

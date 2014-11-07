1c9901dc3e9d503ee4e6f44c4e7b2e296d43ecb7
Joao Campinhos <joao.campinhos@gmail.com>
2014-11-07 12:34:34 +0000

    basic loging

--- server.js
+++ server.js
@@ -4,6 +4,24 @@ var Hapi = require('hapi');
 //Create a server with a host and port
 var server = new Hapi.Server('localhost', 8000);
 
+var options = {
+  reporters: [{
+    reporter: require('good-console'),
+    args:[{ log: '*', request: '*' }]
+  }]
+};
+
+server.pack.register({
+  plugin: require('good'),
+  options: options
+}, function (err) {
+
+  if (err) {
+    console.log(err);
+    return;
+  }
+});
+
 server.views({
   engines: {
     html: require('handlebars')

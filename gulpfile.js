4e05d3e10b47c86ecf39de3ad2ebd2dda3a2010f
joaocampinhos <joao.campinhos@gmail.com>
2014-11-29 13:04:40 +0000

    changed front end framework to bootstrap. still missing new versions of the login and signup forms

--- gulpfile.js
+++ gulpfile.js
@@ -1,22 +1,36 @@
 var gulp = require('gulp');
+var del  = require('del');
 var $ = require('gulp-load-plugins')();
 
 gulp.task('styles', function() {
 
-  return gulp.src('public/_sass/styles.scss')
+  return gulp.src('public/_sass/material.scss')
     .pipe($.sass({
       style: 'nested'
     }))
     .pipe($.autoprefixer('last 1 version'))
+    .pipe($.rename('styles.css'))
     .pipe(gulp.dest('public/'));
 
 });
 
 gulp.task('scripts', function() {
 
+  return gulp.src([
+      'public/_js/jquery.min.js',
+      'public/_js/**/*.js'
+    ])
+    .pipe($.concat('scripts.js'))
+    .pipe(gulp.dest('public/'));
+
 });
 
-gulp.task('clean', function() {
+gulp.task('clean', function(cb) {
+
+  return del([
+    'public/styles.css',
+    'public/scripts.js'
+    ], cb);
 
 });
 
